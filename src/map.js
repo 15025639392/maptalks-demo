3ab9e4e95a93d2eca55ef49b5e5637c905178002
ldy <danyang.liao@quanshi.com>
2021-06-24 11:09:44 +0800

    master

---
+++ src/map.js
@@ -0,0 +1,9 @@
+var map = new maptalks.Map('map', {
+    center: [-0.113049,51.498568],
+    zoom: 14,
+    baseLayer: new maptalks.TileLayer('base', {
+        urlTemplate: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
+        subdomains: ['a','b','c','d'],
+        attribution: '&copy; <a href="http://osm.org">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/">CARTO</a>'
+    })
+});
