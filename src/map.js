b11ebad5d85defe5e0500343b523b185d96dd9bd
ldy <danyang.liao@quanshi.com>
2021-06-28 14:27:14 +0800

    通过矢量图层，添加标记

--- src/map.js
+++ src/map.js
@@ -1,9 +1,11 @@
-var map = new maptalks.Map('map', {
-    center: [-0.113049,51.498568],
-    zoom: 14,
-    baseLayer: new maptalks.TileLayer('base', {
-        urlTemplate: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
-        subdomains: ['a','b','c','d'],
-        attribution: '&copy; <a href="http://osm.org">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/">CARTO</a>'
-    })
-});
+var map = new maptalks.Map("map",{
+     center:     [180,0],
+     zoom:  4,
+     baseLayer : new maptalks.TileLayer("base",{
+         urlTemplate:'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+         subdomains:['a','b','c']
+     }),
+     layers : [
+         new maptalks.VectorLayer('v', [new maptalks.Marker([180, 0])])
+     ]
+})
