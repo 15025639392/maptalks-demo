796e41cacb8740fc9c87b0a09c6ea0abf257ebbc
ldy <danyang.liao@quanshi.com>
2021-06-28 14:30:18 +0800

    center测试

---
+++ src/center.js
@@ -0,0 +1,11 @@
+var map = new maptalks.Map("map",{
+    center:     [400,0],
+    zoom:  4,
+    baseLayer : new maptalks.TileLayer("base",{
+        urlTemplate:'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        subdomains:['a','b','c']
+    }),
+    layers : [
+        new maptalks.VectorLayer('v', [new maptalks.Marker([400, 0])])
+    ]
+})
